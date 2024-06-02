876eaaa70f0899131055fcef2ad099873c4bee9c
Shuto Tanabashi <shuuto570@outlook.com>
2024-06-02 17:12:08 +0900

    add setup shell script for zsh and sheldon

---
+++ sheldon/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# Make simbolic link for sheldon configulation file
+ln -s ~/dotfiles/sheldon ~/.config/sheldon
---
+++ zsh/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Make simbolic link of zsh configulation files
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zsh_envcfg ~/.zsh_envcfg
