f3c8f49b94fd3f2c31bfe7f40213e03307569326
Shuto Tanabashi <tanabashi@s.okayama-u.ac.jp>
2024-07-12 18:35:40 +0900

    nvim: Add alternative shortcut for the cosco extension
    On Windows terminal emulators (such as wt.exe ,alacritty, and wezterm),
    the shrtcut key of <C-;> does not work.
    So alternative shortcut key <A-;> has been added.

--- nvim/lua/extensions.lua
+++ nvim/lua/extensions.lua
@@ -140,6 +140,8 @@ require("lazy").setup({
     keys = {
       { "<C-;>", "<Plug>(cosco-commaOrSemiColon)",       mode = "n", silent = true },
       { "<C-;>", "<Esc><Plug>(cosco-commaOrSemiColon)o", mode = "i", silent = true },
+      { "<A-;>", "<Plug>(cosco-commaOrSemiColon)",       mode = "n", silent = true },
+      { "<A-;>", "<Esc><Plug>(cosco-commaOrSemiColon)o", mode = "i", silent = true },
     },
   },
   {
