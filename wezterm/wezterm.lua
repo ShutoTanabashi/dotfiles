7d1619e63c39a9961dcdcc814094d43b3d342e8a
Shuto Tanabashi <shuuto570@outlook.com>
2026-04-01 00:28:44 +0900

    wezterm: Add inactive pane brightness setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,6 +13,9 @@ config.colors = {
   -- IME background color
   compose_cursor = 'none',
 }
+config.inactive_pane_hsb = {
+  brightness = 0.5,
+}
 config.use_ime = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
