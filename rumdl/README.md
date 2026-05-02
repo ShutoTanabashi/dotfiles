71b457b0a736450b88ddab13531675edccf48948
Shuto Tanabashi <shuuto570@outlook.com>
2026-05-02 22:44:47 +0900

    rumdl: Migrate from markdownlint

---
+++ rumdl/README.md
@@ -0,0 +1,7 @@
+# rumdl 設定ファイル
+
+[rumdl](https://github.com/rvben/rumdl)向け設定ファイル
+
+## ファイル構成
+
+*   `.rumdl.toml` - rumdlの解析オプション
---
+++ rumdl/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Make symbolic link for rumdl settings
+ln -s ~/dotfiles/rumdl/.rumdl.toml ~/.config/rumdl/.rumdl.toml
